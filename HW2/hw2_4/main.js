8c4d2950c8ac0f3345b6dad918cee69a3ec7d898
YuraMyndrya <yuramyndrya7@gmail.com>
2025-10-16 13:31:13 +0300

    hw2_4 done

---
+++ HW2/hw2_4/main.js
@@ -0,0 +1,52 @@
+let users = [
+    {name: 'yura1' , username: 'yuras' , password:'pass1_1'},
+    {name: 'vasya2' , username: 'vasyas' , password:'pass2_2'},
+    {name: 'petya3' , username: 'petyas' , password :'pass3_3'},
+    {name: 'vova4' , username: 'vovas' , password:'pass4_4'},
+    {name: 'anna5' , username: 'annas' , password:'pass5_5'},
+    {name: 'emma6' , username: 'emmas' , password:'pass6_6'},
+    {name: 'nadya7' , username: 'nadyas' , password:'pass7_7'},
+    {name: 'stas8' , username: 'stas8' , password:'pass8_8'},
+    {name: 'taras9' , username: 'taras9' , password:'pass9_9'},
+    {name: 'dmytro10' , username: 'dmytro10' , password:'pass10_10'},
+];
+
+
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+
+
+// let user11 = {name: 'yura11' , username: 'yura111' , password:'p111_11'};
+// let user12 = {name: 'vasya12' , username: 'vasya112' , password:'p112_12'};
+// let user13 = {name: 'petya13' , username: 'petya113' , password:'p113_13'};
+// let user14 = {name: 'vova14' , username: 'vova114' , password:'p114_14'};
+// let user15 = {name: 'anna15' , username: 'anna115' , password:'p115_15'};
+// let user16= {name: 'emma16' , username: 'emma116' , password:'p116_16'};
+// let user17 = {name: 'nadya17' , username: 'nadya117' , password:'p117_17'};
+// let user18 = {name: 'stas18' , username: 'stas118' , password:'p118_18'};
+// let user19 = {name: 'taras19' , username: 'taras119' , password:'p119_19'};
+// let user20 = {name: 'dmytro20' , username: 'dmytro120' , password:'p120_20'};
+//
+//
+// let users = [
+//     user11, user12, user13, user14, user15, user16, user17, user18, user19, user20,
+// ]
+//
+// console.log(users[0].password);
+// console.log(users[1].password);
+// console.log(users[2].password);
+// console.log(users[3].password);
+// console.log(users[4].password);
+// console.log(users[5].password);
+// console.log(users[6].password);
+// console.log(users[7].password);
+// console.log(users[8].password);
+// console.log(users[9].password);
