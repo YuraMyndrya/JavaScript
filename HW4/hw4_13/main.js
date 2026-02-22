d3391c1567dbb1afbae463841e61506c2bb2a4d4
YuraMyndrya <yuramyndrya7@gmail.com>
2026-02-22 12:18:11 +0200

    HW4 (hw4_13 done) 1 more option

--- HW4/hw4_13/main.js
+++ HW4/hw4_13/main.js
@@ -3,7 +3,8 @@
 // Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
 
 
-function exchange(sumUAH, currencyValues, exchangeCurrency) {
+//------------------------------------------------------------1-----------------------------//
+function exchange1(sumUAH, currencyValues, exchangeCurrency) {
     for (let i = 0; i < currencyValues.length; i++) {
         if (currencyValues[i].currency === exchangeCurrency) {
             return sumUAH / currencyValues[i].value;
@@ -13,7 +14,7 @@ function exchange(sumUAH, currencyValues, exchangeCurrency) {
 }
 
 
-console.log(exchange(10000,
+console.log(exchange1(30000,
     [
         {currency: 'USD', value: 25},
         {currency: 'EUR', value: 42},
@@ -21,4 +22,24 @@ console.log(exchange(10000,
         {currency: 'CZK', value: 2},
     ],
     'CZK')
+);
+
+//------------------------------------------------------------2-----------------------------//
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+    for (let item of currencyValues) {
+        if (item.currency === exchangeCurrency) {
+            return sumUAH / item.value;
+        }
+    }
+    return 'incorrect exchangeCurrency';
+}
+
+console.log(exchange(10000,
+    [
+        {currency: 'USD', value: 25},
+        {currency: 'EUR', value: 42},
+        {currency: 'PLN', value: 12},
+        {currency: 'CZK', value: 2},
+    ],
+    'EUR')
 );
