941a1858f8cd515146ec14d1ac28ab49c17c9fdd
YuraMyndrya <yuramyndrya7@gmail.com>
2026-06-23 14:39:56 +0300

    Delete all changes

--- LESSON7/lesson7_1( Функції конструктори)/main.js
+++ LESSON7/lesson7_1( Функції конструктори)/main.js
@@ -6,9 +6,3 @@ function User(name, age, status) {
 
 let user1 = new User('asd', 123, true);
 console.log(user1);
-console.log('notebook3');
-console.log('computer4');
-console.log('notebook6');
-console.log('computer9');
-console.log('computer10');
-console.log('computer11_Remote Log: origin/master');
